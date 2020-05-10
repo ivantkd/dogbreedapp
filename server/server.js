3700c91309b587fa886ff80aff07d373be0335de
ivan <ivan.rubi@e-campus.uab.cat>
2020-05-10 15:09:39 +0200

    client con docker funcionando

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require("path");
 const app = express();
 const PORT = 8080;
 require('./src/database');
@@ -25,4 +26,14 @@ app.use('/apidog', apidogRouter);
 
 app.listen(PORT, function () {
   console.log(`Server Listening on ${PORT}`);
+});
+
+// will redirect all the non-api routes to react frontend
+const CLIENT_BUILD_PATH = path.join(__dirname, "../client/build");
+// Static files
+app.use(express.static(CLIENT_BUILD_PATH));
+
+// Server React Client
+app.get("/", function(req, res) {
+  res.sendFile(path.join(CLIENT_BUILD_PATH , "index.html"));
 });
